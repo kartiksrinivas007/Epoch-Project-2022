38894f8548891efd2bc461ddeadf6058ce0f5677
kartiksrinivas007 <kartiksrinivas008@gmail.com>
2022-06-25 15:16:26 +0530

    Hyperparameter Tuning

--- General_Solver.py
+++ General_Solver.py
@@ -15,7 +15,8 @@ class GeneralSolver():
         self.grad_history = np.array([])
         self.vel_history = np.array([])
         self.loss_final_history = np.array([])
-        self.grad_final_history = np.array([])       
+        self.grad_final_history = np.array([])  
+        self.loss_full_history = np.array([])     
         self.mu = mu
         pass
     def train(self, mode):
@@ -29,13 +30,15 @@ class GeneralSolver():
                 X_batch = self.data['X_train'][j * self.batch_size:(j + 1) * self.batch_size, :]
                 y_batch = self.data['y_train'][j * self.batch_size:(j + 1) * self.batch_size].reshape(-1,1)
                 loss, grads = self.model.loss(X_batch, y_batch)
+                self.loss_full_history = np.append(self.loss_full_history, loss)
                 for param,value in self.model.params.items():
                     if mode == 'sgd':
                         self.sgd_update(param,grads[param])
                     elif mode == 'sgd_momentum':
                         self.momentum_update(param,grads[param])
+                    
                         
-                if(j  == 0):
+                if(j%self.print_every ==  0):
                     print("Epoch = ", i, "Batch = ", j, "Loss = ", loss)
                     self.loss_history = np.append(self.loss_history, loss)
                     # self.grad_history = np.append(self.grad_history, np.linalg.norm(grads['W1']))            
--- Models/Classifiers/Neural_Net.py
+++ Models/Classifiers/Neural_Net.py
@@ -2,14 +2,14 @@ import numpy as np
 from ..Layers import *
 
 class TwoLayerNet():
-    def __init__(self, input_dim, hidden_dim, output_dim, reg = 0.01):
+    def __init__(self, input_dim, hidden_dim, output_dim, reg = 0.01, weight_scale = 1e-3):
         self.reg = reg
         self.params = {}
         # Now we do intialization of the weights
         np.random.seed(0)
-        self.params['W1'] = np.random.randn(input_dim, hidden_dim)*0.01
+        self.params['W1'] = np.random.randn(input_dim, hidden_dim)*weight_scale#np.sqrt(2.0/input_dim)
         self.params['b1'] = np.zeros(hidden_dim)
-        self.params['W2'] = np.random.randn(hidden_dim, output_dim)*0.01
+        self.params['W2'] = np.random.randn(hidden_dim, output_dim)*weight_scale#np.sqrt(2.0/hidden_dim)
         self.params['b2'] = np.zeros(output_dim)
         self.output_dim = output_dim
 
@@ -27,18 +27,21 @@ class TwoLayerNet():
             da1, grads['W2'],grads['b2'] = affine_backward(da2,cache['affine_2'])
             dz1 = relu_backward(da1, cache['relu_1'])
             dx, grads['W1'],grads['b1'] = affine_backward(dz1, cache['affine_1'])
-            loss = loss + self.reg * (np.sum(self.params['W1']**2) + np.sum(self.params['W2']**2))
-            grads['W1'] = grads['W1'] + 2 * self.reg * self.params['W1']
-            grads['W2'] = grads['W2'] + 2 * self.reg * self.params['W2']
+            loss = loss + 0.5 * self.reg * (np.sum(self.params['W1']**2) + np.sum(self.params['W2']**2))
+            grads['W1'] = grads['W1'] + 1 * self.reg * self.params['W1']
+            grads['W2'] = grads['W2'] + 1 * self.reg * self.params['W2']
             return loss, grads
         else:
+            cache = {}
+            loss = 0
             z1, cache['affine_1'] = affine_forward(X,self.params['W1'],self.params['b1'])
             a1,cache['relu_1'] = relu_forward(z1)
             a2,cache['affine_2'] = affine_forward(a1,self.params['W2'], self.params['b2'])
-            loss, da2 = softmax_loss(a2,y)
-            loss = loss + self.reg * (np.sum( (self.params['W1'] * self.params['W1'])))
-            loss = loss + self.reg * (np.sum( (self.params['W2'] * self.params['W2'])))
-            return loss
+            return a2
+            # loss, da2 = softmax_loss(a2,y)
+            # loss = loss + self.reg * (np.sum( (self.params['W1'] * self.params['W1'])))
+            # loss = loss + self.reg * (np.sum( (self.params['W2'] * self.params['W2'])))
+            # return loss
     def predict(self, X):
         cache= {}
         z1, cache['affine_1'] = affine_forward(X,self.params['W1'],self.params['b1'])
